8087dfbc2cf2a11cfaf752fe3073c524da674d9e
Ruslan Jankurazov <ruslanjan888@gmail.com>
2017-04-08 17:00:42 +0600

    init package

---
+++ Protium/src/net/protium/Protium.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2017 Ruslan Jankurazov, Dmitry Ussoltsev - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
+package net.protium;
+
+public class Protium {
+    public static void Main(String args[]) {
+
+    }
+}
