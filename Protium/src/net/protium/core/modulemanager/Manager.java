5dcd3ad7c78df4c5750cca108391acc39b3c4bd6
Karl Meinkopf <kremen.karl@yandex.ru>
2017-04-09 22:23:21 +0600

    Added: $.Protium -> HelloWorld web-server.

--- Protium/src/net/protium/core/modulemanager/Manager.java
+++ Protium/src/net/protium/core/modulemanager/Manager.java
@@ -5,60 +5,54 @@
  */
 
 package net.protium.core.modulemanager;
-
-
 import net.protium.api.Constant;
 import net.protium.api.agents.CoreAgent;
 import net.protium.api.agents.ModuleManager;
 import net.protium.api.module.Module;
 import net.protium.core.utils.Functions;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
 
-import static java.io.File.separator;
-
+@SuppressWarnings("unused")
 public class Manager implements ModuleManager {
 
-    private Map<String, Module> modules;
+	private Map < String, Module > modules;
 
-    public Manager() {
-        CoreAgent.setModuleManager(this);
-        modules = new HashMap<>();
-        loadNotExistingModules();
-    }
+	@SuppressWarnings("unused")
+	public Manager( ) {
+		CoreAgent.setModuleManager(this);
+		modules = new HashMap <>();
+		loadNotExistingModules();
+	}
 
-    @SuppressWarnings("WeakerAccess")
-    public void loadNotExistingModules() {
+	@SuppressWarnings("WeakerAccess")
+	public void loadNotExistingModules( ) {
 
-    }
+	}
 
-    @SuppressWarnings("WeakerAccess")
-    public void unloadModule(String name) {
-        if (!isLoaded(name)) {
-            modules.get(name).onDisable();
-            modules.remove(name);
-        }
-    }
+	@SuppressWarnings("WeakerAccess")
+	public void unloadModule(String name) {
+		if (!isLoaded(name)) {
+			modules.get(name).onDisable();
+			modules.remove(name);
+		}
+	}
 
-    public void loadModule(String name) throws IOException, ClassNotFoundException, IllegalAccessException, InstantiationException {
-        String pathToJar = Functions.pathToFile(Constant.getMOD_D(), name, Constant.getMOD_EXT());
-        System.err.println(pathToJar);
-        JarFile jarFile = new JarFile(pathToJar);
-        URL[] urls = { new URL("jar:file:" + pathToJar+"!/") };
-        URLClassLoader cl = URLClassLoader.newInstance(urls);
+	@SuppressWarnings("WeakerAccess")
+	public void loadModule(String name) throws IOException, ClassNotFoundException, IllegalAccessException, InstantiationException {
+		String pathToJar = Functions.pathToFile(Constant.getMOD_D(), name, Constant.getMOD_EXT());
+		System.err.println(pathToJar);
+		URL[] urls = { new URL("jar:file:" + pathToJar + "!/") };
+		URLClassLoader cl = URLClassLoader.newInstance(urls);
 
-        Class c = cl.loadClass(name);
-        Module newModule = ((Module)c.newInstance());
-        newModule.onEnable();
-        modules.put(name, newModule);
+		Class c = cl.loadClass(name);
+		Module newModule = ((Module) c.newInstance());
+		newModule.onEnable();
+		modules.put(name, newModule);
 
 
         /*
@@ -78,19 +72,20 @@ public class Manager implements ModuleManager {
             //nw.onEnable();
         }
         */
-    }
+	}
 
-    public void reloadModule(String name) throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException {
-        unloadModule(name);
-        loadModule(name);
-    }
+	@SuppressWarnings("unused")
+	public void reloadModule(String name) throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+		unloadModule(name);
+		loadModule(name);
+	}
 
-    private boolean isLoaded(String name) {
-        return modules.containsKey(name);
-    }
+	private boolean isLoaded(String name) {
+		return modules.containsKey(name);
+	}
 
-    @Override
-    public Module getModule(String name) {
-        return modules.get(name);
-    }
+	@Override
+	public Module getModule(String name) {
+		return modules.get(name);
+	}
 }
