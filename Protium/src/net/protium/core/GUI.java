67639ee57d0e0dd27b643c2fb634b5509a511bab
Ruslan Jankurazov <ruslanjan@MacBook-Pro-Ruslan.local>
2017-04-15 14:52:52 +0600

    Deleted _mac

---
+++ Protium/src/net/protium/core/GUI.java
@@ -0,0 +1,10 @@
+package net.protium.core;
+
+/**
+ * Copyright (C) Ruslan Jankurazov - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Ruslan Jankurazov <ruslanjan@gmail.com>, 15.04.17
+ */
+public class GUI {
+}
